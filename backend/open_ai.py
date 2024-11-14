f1a1171308affdfb5ae9a1c7cf0cabc75520193a
HeoTaeRyang <cake0702@naver.com>
2024-11-14 20:20:15 +0900

    파일 위치 변경

--- backend/open_ai.py
+++ backend/open_ai.py
@@ -3,7 +3,7 @@ import os
 from getpass import getpass
 
 # API 키를 환경 변수에 저장
-# os.environ['OPENAI_API_KEY'] = "API키 입력"
+os.environ['OPENAI_API_KEY'] = ""
 
 def create_chat_completion(user_input):
     system_input = "넌 프로그래밍 전문 강사아. 프로그래밍에 관련된 질문에만 친절하고 간략하게 답변해줘"
