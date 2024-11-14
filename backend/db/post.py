2192979454d095799554f78a13f3007c971b08b1
HeoTaeRyang <cake0702@naver.com>
2024-11-14 20:21:04 +0900

    자유게시판 db 컨트롤러ㅓ 수정

--- backend/db/post.py
+++ backend/db/post.py
@@ -1,24 +1,45 @@
 from connect import con
 cursor = con.cursor()
 
+MAX_PAGE = 10
+
+def get_post_current_num():
+    cursor.execute("SELECT MAX(Number) FROM Post;")
+    res = cursor.fetchone()[0]
+    if res:
+        return res
+    else:
+        return 0
+
 def get_post_num():
-    cursor.execute("SELECT Post_num FROM Post_Nums WHERE Post_Type = 'Post';")
+    cursor.execute("SELECT COUNT(*) FROM Post WHERE Is_Del = 0;")
     return cursor.fetchone()[0]
 
-def get_all_post():
-    cursor.execute("SELECT * FROM Post WHERE Is_Del = 0;")
+#페이지 개수 리턴
+def get_max_page():
+    num = get_post_num()
+    if num % MAX_PAGE != 0:
+        return num // MAX_PAGE + 1
+    else:
+        return num // MAX_PAGE
+
+#입력한 페이지의 글들 리턴
+def get_page_post(page):
+    cursor.execute(f"SELECT Number,Title,Writer_id,Datetime,View_Count,Recommend_Count FROM Post WHERE Is_Del = 0 ORDER BY Number DESC LIMIT {MAX_PAGE} OFFSET {page-1} * {MAX_PAGE};")
     return cursor.fetchall()
 
-def get_post(num):
-    cursor.execute(f"SELECT * FROM Post WHERE Number={num};")
+#입력한 번호의 글 content 리턴
+def get_content_post(num):
+    cursor.execute(f"SELECT Content FROM Post WHERE Number={num};")
     return cursor.fetchone()
 
+#글 추가
 def add_post(title,writer_id,datetime,content):
-    cursor.execute(f"UPDATE Post_Nums SET Post_Num = Post_Num + 1 WHERE Post_Type = 'Post';")
-    num = get_post_num()
-    cursor.execute(f"INSERT INTO Post VALUES('{num}','{title}','{writer_id}','{datetime}',0,0,'{content}',0);")
+    num = get_post_current_num()
+    cursor.execute(f"INSERT INTO Post VALUES('{num+1}','{title}','{writer_id}','{datetime}',0,0,'{content}',0);")
     con.commit()
 
+#글 삭제
 def del_post(num):
     cursor.execute(f"UPDATE Post SET Is_Del = 1 WHERE Number='{num}';")
-    con.commit()   
+    con.commit()
