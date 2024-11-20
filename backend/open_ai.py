66cc9dc209b5f531a85ad38fa73ec592084a5c6d
HeoTeaRyang <cake0702@naver.com>
2024-11-20 22:00:13 +0900

    불필요 코드 삭제

--- backend/open_ai.py
+++ backend/open_ai.py
@@ -3,11 +3,7 @@ import os
 from getpass import getpass
 
 # API 키를 환경 변수에 저장
-<<<<<<< HEAD:backend/ai/open_ai.py
 os.environ['OPENAI_API_KEY'] = "API키 입력"
-=======
-os.environ['OPENAI_API_KEY'] = ""
->>>>>>> 65c4bc6f8f2cdc108a2dec0340f18842625bcce1:backend/open_ai.py
 
 def create_chat_completion(user_input):
     system_input = "넌 프로그래밍 전문 강사아. 프로그래밍에 관련된 질문에만 친절하고 간략하게 답변해줘"
