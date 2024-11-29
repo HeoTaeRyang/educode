49223c0ea446dc31eab45a01278fc4b3f6d5fe8c
HeoTaeRyang <cake0702@naver.com>
2024-11-29 21:50:00 +0900

    함수 이름 변경

--- backend/db/comment.py
+++ backend/db/comment.py
@@ -1,7 +1,7 @@
 from db.connect import con
 
 #댓글 개수 리턴
-def get_comment_num_post(post_type, num):
+def get_comment_num(post_type, num):
     cursor = con.cursor()
     cursor.execute(f"SELECT Count(*) FROM Comment WHERE Post_Type = '{post_type}' and Post_Number={num};")
     res = cursor.fetchone()[0]
@@ -11,7 +11,7 @@ def get_comment_num_post(post_type, num):
         return 0
 
 #댓글들 리턴
-def get_comment_post(post_type,num):
+def get_comment(post_type,num):
     cursor = con.cursor()
     cursor.execute(f"SELECT Writer_id,Datetime,Content FROM Comment WHERE Post_Type = '{post_type}' and Post_Number={num};")
     return cursor.fetchall()
